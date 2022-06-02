5c04838479ad831de94d411c00d22b30f38d81b0
Allen <a.ye@refinitiv.com>
2022-06-02 15:32:03 +0800

    init

---
+++ src/components/MultiCheck.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>@todo</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export type Option = {
+	label: string;
+	value: string;
+};
+
+/**
+ * Notice:
+ * 1. There should be a special `Select All` option with checkbox to control all passing options
+ * 2. If columns > 1, the options should be placed from top to bottom in each column
+ *
+ * @param {string} label - the label text of this component
+ * @param {Option[]} options - options
+ * @param {string[]} values - default checked option values
+ * @param {number} columns - default value is 1
+ * @param {Function} onChange - when checked options are changed,
+ *                             they should be passed to outside
+ */
+
+export default defineComponent({
+  name: 'MultiCheck',
+});
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Multi Check Component</h1>
+  <MultiCheck />
+  <div>
+    <h2>Current selected values:</h2>
+    <div>{{ selectedValues.join(",") }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MultiCheck, { Option } from "@/components/MultiCheck.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    MultiCheck,
+  },
+  data() {
+    return {
+      options: [
+        { label: "aaa", value: "111" },
+        { label: "bbb", value: "222" },
+        { label: "ccc", value: "333" },
+        { label: "ddd", value: "444" },
+        { label: "eee", value: "555" },
+        { label: "Under Agreement", value: "666" },
+        { label: "ggg", value: "777" },
+        { label: "hhh", value: "888" },
+        { label: "iii", value: "999" },
+      ] as Option[],
+      selectedValues: [] as string[],
+    };
+  },
+});
+</script>
