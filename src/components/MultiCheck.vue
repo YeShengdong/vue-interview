eae4b3c049169b6fe508758e2eb895a8f2d8fdd3
zouxinxian <zouxx09@chinatelecom.cn>
2022-07-06 19:11:30 +0800

    MultiCheck components

--- src/components/MultiCheck.vue
+++ src/components/MultiCheck.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>@todo</div>
+  <div class="multi-check">
+    <Card>
+      <template #title>
+        {{ title }}
+      </template>
+      <div class="card-content">
+        <Checkbox :model-value="checkAll" @on-change="handleCheckAll"
+          >Select All</Checkbox
+        >
+        <Row>
+          <Col
+            :span="columns"
+            v-for="(ele, index) in optionsList"
+            :key="index"
+          >
+            <CheckboxGroup
+              v-model="checkGroup"
+              @on-change="checkGroupChange"
+            >
+              <Checkbox
+                v-for="item in ele"
+                :key="item.value"
+                :label="item.label"
+                class="checkbox"
+                >{{ item.label }}</Checkbox
+              >
+            </CheckboxGroup>
+          </Col>
+        </Row>
+      </div>
+    </Card>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export type Option = {
-	label: string;
-	value: string;
+  label: string;
+  value: string;
 };
 
 /**
@@ -17,16 +48,83 @@ export type Option = {
  *
  * @param {string} label - the label text of this component
  * @param {Option[]} options - options
- * @param {string[]} values - default checked option values
+ * @param {string[]} checkAllGroup - default checked option values
  * @param {number} columns - default value is 1
  * @param {Function} onChange - when checked options are changed,
  *                             they should be passed to outside
  */
 
 export default defineComponent({
-  name: 'MultiCheck',
+  name: "MultiCheck",
+  data() {
+    return {
+      checkAll: false,
+      checkGroup: [] as Option[],
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      default: "多选组件",
+    },
+    options: {
+      type: Array,
+      default: [] as Option[],
+    },
+  },
+  computed: {
+    optionsList() {
+      let newArr: any = [];
+      for (var i = 0; i < this.options.length; i += 5) {
+        newArr.push(this.options.slice(i, i + 5));
+      }
+      return newArr;
+    },
+    columns(){
+      return Math.floor(24 / this.optionsList.length)
+    }
+  },
+  methods: {
+    handleCheckAll(checked: boolean) {
+      this.checkAll = checked;
+      if (this.checkAll) {
+        this.options.forEach((item: any) => {
+          this.checkGroup.push(item.label);
+        });
+      } else {
+        this.checkGroup = [];
+      }
+      this.$emit("getCheckGroup", this.checkGroup);
+    },
+    checkGroupChange(checkGroup: string[]) {
+      if (checkGroup.length == this.options.length) {
+        this.checkAll = true;
+      } else {
+        this.checkAll = false;
+      }
+      this.$emit("getCheckGroup", this.checkGroup);
+    },
+  },
 });
 </script>
 
 <style scoped lang="less">
+.multi-check {
+  width: 30%;
+  margin: 0 auto;
+  padding: 10px 0;
+  .card-content {
+    height: 200px;
+    text-align: left;
+
+    .checkbox {
+      display: block;
+      text-align: left;
+    }
+  }
+}
+/deep/ .ivu-card-head {
+  background: #eee;
+  text-align: left;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Multi Check Component</h1>
-  <MultiCheck />
+  <MultiCheck :title="'Status'" :options='options' @getCheckGroup='getCheckGroup'/>
   <div>
     <h2>Current selected values:</h2>
     <div>{{ selectedValues.join(",") }}</div>
@@ -24,13 +24,21 @@ export default defineComponent({
         { label: "ccc", value: "333" },
         { label: "ddd", value: "444" },
         { label: "eee", value: "555" },
-        { label: "Under Agreement", value: "666" },
+        { label: "Under", value: "666" },
         { label: "ggg", value: "777" },
         { label: "hhh", value: "888" },
         { label: "iii", value: "999" },
+        { label: "kkk", value: "929" },
+        { label: "lll", value: "939" },
+        { label: "mmm", value: "949" },
       ] as Option[],
       selectedValues: [] as string[],
     };
   },
+  methods:{
+    getCheckGroup(data: string[]){
+      this.selectedValues= data
+    }
+  }
 });
 </script>
