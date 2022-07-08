1be2a3a91b997c610b80bcb9c1f2c2f747774148
杨宏伟 <yanghongwei@yanghongweideMacBook-Pro.local>
2022-07-08 13:16:50 +0800

    test

--- src/components/MultiCheck.vue
+++ src/components/MultiCheck.vue
@@ -1,5 +1,19 @@
 <template>
     <div>@todo</div>
+    <p style="margin: auto;text-align: center; border: 1px solid #000; border-bottom: none;" :style="{width: containerWidth}">
+      {{label}}
+    </p>
+    <div :style="{width: containerWidth, margin: 'auto'}" class="container">
+      <div :key="columnIndex" v-for="(columnItem, columnIndex) in checkboxData" :style="{height: columnHeight}">
+        <p v-if="columnIndex === 0">
+          <input type="checkbox" :checked="checkedAll" @click="selectAll"> selectAll
+        </p>
+         <p :key="item.value" v-for="item in columnItem">
+        
+           <input  type="checkbox" :value="item.value"   v-model="selectValues"> {{item.label}}
+        </p>
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -25,8 +39,122 @@ export type Option = {
 
 export default defineComponent({
   name: 'MultiCheck',
+  props: {
+    label: {
+      type: String,
+      default: 'select'
+    },
+    options: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    values: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    number: {
+      type: Number,
+      default: 2
+    }
+  },
+  data () {
+    return {
+       checkboxData: [] as any[],
+       //values: []
+       checkedAll: false,
+       selectValues: [] as any[],
+       columnHeight: ''
+    } 
+  },
+  created() {
+    this.initData()
+  },
+  watch: {
+    selectValues(newVal) {
+      if(newVal.length === this.options.length) {
+         this.checkedAll = true
+      }else {
+        this.checkedAll = false
+      }
+      this.$emit('onChange', newVal)
+    }
+  },
+  methods: {
+    initData() {
+       this.selectValues = this.values
+       let colCount = Math.ceil(this.options.length / this.number)
+       this.columnHeight =  (colCount + 1) * 22 + 'px'
+       if(colCount >= this.options.length) {
+         this.checkboxData.push([...this.options])
+       }else {
+         let cur = 0
+         while(cur < this.options.length) {
+           if(cur + colCount < this.options.length) {
+             this.checkboxData.push([...this.options.slice(cur, cur + colCount)])
+           }else {
+             this.checkboxData.push([...this.options.slice(cur, this.options.length)])
+           }
+            cur = cur + colCount
+         }
+       }
+       while(this.checkboxData.length < this.number) {
+         this.checkboxData.push([])
+       }
+    },
+    selectAll (e: Event) {
+    let checked = (e.target as HTMLInputElement).checked
+    if(checked){
+        this.selectValues = this.options.map( (item:any) => {
+          
+          return item.value
+      }) 
+        
+     }else {
+       this.selectValues = []
+     }
+     this.$emit('onChange', this.selectValues)
+
+  
+    }
+  },
+  computed: {
+    containerWidth () {
+      return this.number * 300 + 'px'
+    }
+  }
+
 });
 </script>
 
 <style scoped lang="less">
+p {
+  margin: 0;
+}
+.container{
+  &::after{
+    content: '';
+    clear: both;
+    display: block;
+  }
+  div {
+    width: 300px;
+    float: left;
+    box-sizing: border-box;
+    border: 1px solid #000;
+    p{
+      
+      text-align: left;
+      text-overflow: ellipsis;
+      overflow:hidden;
+      white-space: nowrap;
+    }
+  }
+  div:nth-child(n + 2){
+    border-left: none;
+  }
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-07-08 10:43:04
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-07-08 11:09:10
+ * @FilePath: /vue-interview/src/main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
